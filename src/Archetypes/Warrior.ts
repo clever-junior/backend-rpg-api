b2082873c550257e244fabf88ccc963799bf696d
clever-junior <tk_clever.junior@outlook.com>
2022-10-25 16:25:58 -0400

    feat: requisito 6

--- src/Archetypes/Warrior.ts
+++ src/Archetypes/Warrior.ts
@@ -3,18 +3,18 @@ import Archetype from './Archetype';
 
 export default class Warrior extends Archetype {
   private _energyType: EnergyType = 'stamina';
-  private static instance = 0;
+  private static _instance = 0;
 
   get energyType(): EnergyType {
     return this._energyType;
   } 
 
   static createdArchetypeInstances(): number {
-    return this.instance;
+    return this._instance;
   }
 
   constructor(name: string) {
     super(name);
-    Warrior.instance += 1;
+    Warrior._instance += 1;
   }
 }
