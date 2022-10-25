ed370508a867e4fb5ba5d25c08d88522858be415
clever-junior <tk_clever.junior@outlook.com>
2022-10-25 16:03:02 -0400

    feat: adicioan requisito 4 - refac: refatora a classe Race

---
+++ src/Archetypes/Archetype.ts
@@ -0,0 +1,21 @@
+import { EnergyType } from '../Energy';
+
+export default abstract class Archetype {
+  get special(): number { return this._special; }
+
+  get cost(): number { return this._cost; }
+
+  get name(): string { return this._name; }
+  
+  static createdArchetypeInstances(): number {
+    throw new Error('Not implemented');
+  }
+  
+  abstract get energyType(): EnergyType;
+  
+  constructor(
+    private _name: string,
+    private _special = 0,
+    private _cost = 0,
+  ) {}
+}
--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -1,19 +1,16 @@
 export default abstract class Race {
-  private _name: string;
-  private _dexterity: number;
-
   get name(): string { return this._name; }
 
   get dexterity(): number { return this._dexterity; }
 
-  constructor(name: string, dexterity: number) {
-    this._name = name;
-    this._dexterity = dexterity;
-  } 
-  
   static createdRacesInstances(): number {
     throw new Error('Not implemented');
   }
-
+  
   abstract get maxLifePoints(): number;
+  
+  constructor(
+    private _name: string,
+    private _dexterity: number,
+  ) { } 
 }
